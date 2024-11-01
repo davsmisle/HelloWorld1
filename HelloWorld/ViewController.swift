39ae2c424e4656a9805502f33aca3d65c22b4cab
Dinar Mukhlisov <damukhlisov@gmail.com>
2024-11-01 15:12:06 +0300

    Prepare project/ Deleted all templates comments

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,9 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    final override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
